d4e72ec483687bdab3445e92627b79cfee7e455b
khai96_ <hvksmr1996@gmail.com>
2016-09-21 02:01:11 +0700

    Use wget instead of curl

--- sh/download.sh
+++ sh/download.sh
@@ -3,7 +3,7 @@ source ./sh/include/download-url.sh
 source ./sh/subscripts/create-directories.sh
 
 echo "Downloading Archive from $DOWNLOAD_URL..."
-curl -XGET $DOWNLOAD_URL > ./out/archive.7z || fail "Downloading Archive"
+wget -O ./out/archive.7z $DOWNLOAD_URL || fail "Downloading Archive"
 
 echo "Extracting ./out/archive.7z..."
 7zr e ./out/archive.7z -o./out/src || fail "Extracting Archive"
